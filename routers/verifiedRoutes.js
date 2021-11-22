83e9c7405fa1b4d9f889812d5c3631107f9684aa
Sagnik Haldar <sagnikh2@gmail.com>
2021-11-23 00:37:40 +0530

    added user model SVG file

--- routers/verifiedRoutes.js
+++ routers/verifiedRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const requireAuth = require("../middlewares/authMiddleware");
 
 router.get("/", requireAuth, (req, res) => {
-    res.status(200).send("Acessing verified route");
+	res.status(200).send("Acessing verified route");
 });
 
 module.exports = router;
